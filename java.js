f2bb02dc5350dbef4a1d5db4693bc2e95ab3abe5
moabdelaziem123 <144205494+moabdelaziem123@users.noreply.github.com>
2024-01-28 18:51:03 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav');
+
+
+menu.onclick = () => {
+menu.classList.toggle('bx-x');
+nav.classList.toggle('open');
+
+}
